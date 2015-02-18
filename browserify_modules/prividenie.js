9cdaeaf4472378422fad7700aca462aa45665af2
22century <tnkussr+github@gmail.com>
2015-02-19 00:13:07 +0900

    ちゃんとリセット

--- browserify_modules/prividenie.js
+++ browserify_modules/prividenie.js
@@ -15,18 +15,27 @@ Prividenie.prototype = {
 
     change: function (name) {
         _imgAvatar.src = 'images/ajipoko/' + name + '.png';
+    },
+
+    resetTimer: function () {
         prividenie.clearTimer();
         prividenie.setTimer();
     },
 
     setTimer: function () {
+        if (_timerId > 0) {
+            prividenie.clearTimer();
+        }
         _timerId = setTimeout(function(){
+            _timerId = 0;
             prividenie.random();
             prividenie.setTimer();
         }, 5 * 1000);
+        console.log('setTimer:', _timerId);
     },
 
     clearTimer: function () {
+        console.log('clearTimer:', _timerId);
         clearTimeout(_timerId);
     },
 
@@ -34,6 +43,7 @@ Prividenie.prototype = {
         var msgs = MESSAGES.RANDOM.sample();
         prividenie.change(msgs[0]);
         _balloon.open(msgs[1]);
+        prividenie.resetTimer();
     },
 
     mac: function () {
